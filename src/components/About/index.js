d783bd433acc6e75221a353b98f396faf9b0510f
Oliver Abraham <olly_abraham@hotmail.com>
2020-01-03 15:38:30 +0000

    Simple About Component

---
+++ src/components/About/index.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+import { StyledAbout, StyledAboutBody } from './styles'
+
+const About = () => {
+  return (
+    <StyledAbout>
+      <h2>Hi, i'm Oliver Abraham. <br/>A software engineer from Sheffield UK. </h2>
+      <StyledAboutBody>
+        Interested in working together? Feel free to contact me.
+      </StyledAboutBody>
+    </StyledAbout>
+  )
+}
+
+export default About
---
+++ src/components/About/styles.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components'
+
+export const StyledAbout = styled.div`
+  & h2 {
+    -webkit-font-smoothing: antialiased;
+    font-size: 1.5rem;
+  }
+`;
+
+export const StyledAboutBody = styled.div`
+  margin-top: 1.25rem;
+`;
